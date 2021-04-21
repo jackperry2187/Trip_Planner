100c93b5650a587825df3cef95c4ce9a6ce7cdd5
Jackson Perry <jackperry2187@gmail.com>
2021-04-21 17:37:17 -0400

    Database stuff

--- config/mongoConnection.js
+++ config/mongoConnection.js
@@ -4,13 +4,12 @@ const mongoConfig = settings.mongoConfig;
 
 let _connection = undefined;
 let _db = undefined;
+let uri = "mongodb+srv://jwolfcrafter:Ranger02!@cluster0.miex7.mongodb.net/test?retryWrites=true&w=majority"
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = async () => {
   if (!_connection) {
-    _connection = await MongoClient.connect(mongoConfig.serverUrl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    _connection = await client.connect();
     _db = await _connection.db(mongoConfig.database);
   }
 
