5dcbe9ca13861c7826a889548b75b7b92053c7a1
Jackson Perry <jackperry2187@gmail.com>
2021-04-22 16:19:28 -0400

    Remove login requirement

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,24 @@
-const login = require('./login');
-const register = require('./register');
 const trips = require('./trips');
 const activities = require('./activities');
 const profile = require('./profile');
+let userData = require('../data/users');
 
 const constructorMethod = (app) => {
-  app.use('/login', login);
-  app.use('/register', register);
   app.use('/trips', trips);
   app.use('/activities', activities);
   app.use('/profile', profile);
 
-  app.get('/', (req, res) => {
-    if(req.session.user) {
-      res.redirect('trips');
+  app.get('/', async (req, res) => {
+    if(!req.session.user) {
+      const user = await userData.create("exampleuser", "yourpassword");
+      if(user) {
+          req.session.user = { username: "exampleuser", _id: user._id.toString() };
+          let theTrips = user.trips;
+          if(theTrips) req.session.user.trips = theTrips;
+      }
     }
-    res.render('index', { title: "Trip Planner", form_action: "login", index: true });
+    res.redirect('trips');
+    return;
   });
 
   app.use('*', (req, res) => {
